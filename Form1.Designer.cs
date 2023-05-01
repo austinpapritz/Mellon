867e80f6615bb449005e305dafb6e586fabc88c5
Austin Papritz <austin@papritz.dev>
2023-04-30 17:58:01 -0700

    added TextBoxes and Labels

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            website = new Label();
+            username = new Label();
+            password = new Label();
             SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(34, 114);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(246, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(34, 177);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(246, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(34, 242);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(246, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            website.AutoSize = true;
+            website.Location = new Point(34, 94);
+            website.Name = "label1";
+            website.Size = new Size(38, 15);
+            website.TabIndex = 3;
+            website.Text = "label1";
+            website.Click += label1_Click;
+            // 
+            // label2
+            // 
+            username.AutoSize = true;
+            username.Location = new Point(34, 158);
+            username.Name = "label2";
+            username.Size = new Size(38, 15);
+            username.TabIndex = 4;
+            username.Text = "label2";
+            // 
+            // label3
+            // 
+            password.AutoSize = true;
+            password.Location = new Point(34, 224);
+            password.Name = "label3";
+            password.Size = new Size(38, 15);
+            password.TabIndex = 5;
+            password.Text = "label3";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(670, 410);
+            Controls.Add(password);
+            Controls.Add(username);
+            Controls.Add(website);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label website;
+        private Label username;
+        private Label password;
     }
 }
